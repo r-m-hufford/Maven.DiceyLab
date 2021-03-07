2b1c96a239f4e943f3bb3219c93f95ae7a739c62
r-m-hufford <ryan@rmhufford.com>
2021-03-07 15:02:55 -0500

    simulation nearly complete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main (String[] args) {
         Dice dice = new Dice();
         Bins results = new Bins();
-        Simulation sim = new Simulation(2,100);
+        Simulation sim = new Simulation(2,1000000);
 
         sim.runSimulation();
         sim.printResults();
--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class Simulation {
     Dice dice = new Dice();
     Bins bin = new Bins();
@@ -26,61 +27,63 @@ public class Simulation {
             simulationMemory.add(thisRoll);
             rolls--;
         }
-        System.out.println("Total: " + total);
         return total;
     }
 
     public void printResults() {
-        System.out.println(simulationMemory);
 
-        String twos = "";
-        String threes = "";
-        String fours = "";
-        String fives = "";
-        String sixes = "";
-        String sevens = "";
-        String eights = "";
-        String nines = "";
-        String tens = "";
-        String elevens = "";
-        String twelves = "";
+        int[] simResults = new int[11];
 
         for (int i = 0; i < simulationMemory.size(); i++) {
             if (simulationMemory.get(i) == 2) {
-                twos += "*";
+                simResults[0]++;
             } else if (simulationMemory.get(i) == 3) {
-                threes += "*";
+                simResults[1]++;
             } else if (simulationMemory.get(i) == 4) {
-                fours += "*";
+                simResults[2]++;
             } else if (simulationMemory.get(i) == 5) {
-                fives += "*";
+                simResults[3]++;
             } else if (simulationMemory.get(i) == 6) {
-                sixes += "*";
+                simResults[4]++;
             } else if (simulationMemory.get(i) == 7) {
-                sevens += "*";
+                simResults[5]++;
             } else if (simulationMemory.get(i) == 8) {
-                eights += "*";
+                simResults[6]++;
             } else if (simulationMemory.get(i) == 9) {
-                nines += "*";
+                simResults[7]++;
             } else if (simulationMemory.get(i) == 10) {
-                tens += "*";
+                simResults[8]++;
             } else if (simulationMemory.get(i) == 11) {
-                elevens += "*";
+                simResults[9]++;
             } else {
-                twelves += "*";
+                simResults[10]++;
             }
         }
-            System.out.println(twos);
-            System.out.println(threes);
-            System.out.println(fours);
-            System.out.println(fives);
-            System.out.println(sixes);
-            System.out.println(sevens);
-            System.out.println(eights);
-            System.out.println(nines);
-            System.out.println(tens);
-            System.out.println(elevens);
-            System.out.println(twelves);
+
+        String two = String.format("% d :   % d:  %.2f **", 2, simResults[0], (float)simResults[0]/1000000);
+        String three = String.format("% d :   % d:  %.2f *****", 3, simResults[1], (float)simResults[1]/1000000);
+        String four = String.format("% d :   % d:  %.2f ********", 4, simResults[2], (float)simResults[2]/1000000);
+        String five = String.format("% d :   %d:  %.2f ***********", 5, simResults[3], (float)simResults[3]/1000000);
+        String six = String.format("% d :   %d:  %.2f *************", 6, simResults[4], (float)simResults[4]/1000000);
+        String seven = String.format("% d :   %d:  %.2f ****************", 7, simResults[5], (float)simResults[5]/1000000);
+        String eight = String.format("% d :   %d:  %.2f *************", 8, simResults[6], (float)simResults[6]/1000000);
+        String nine = String.format("% d :   %d:  %.2f ***********", 9, simResults[7], (float)simResults[7]/1000000);
+        String ten = String.format("%d :   % d:  %.2f ********", 10, simResults[8], (float)simResults[8]/1000000);
+        String eleven = String.format("%d :   % d:  %.2f *****", 11, simResults[9], (float)simResults[9]/1000000);
+        String twelve = String.format("%d :   % d:  %.2f **", 12, simResults[10], (float)simResults[10]/1000000);
+
+
+        System.out.println(two);
+        System.out.println(three);
+        System.out.println(four);
+        System.out.println(five);
+        System.out.println(six);
+        System.out.println(seven);
+        System.out.println(eight);
+        System.out.println(nine);
+        System.out.println(ten);
+        System.out.println(eleven);
+        System.out.println(twelve);
 
     }
 }
